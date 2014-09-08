33f57494846a6df58d58ed94430789a24e574899
David Sypien <davidsypien@gmail.com>
2014-09-07 21:56:17 -0400

    use jquery to post form, allows me to set header info

--- piserver/public/javascripts/piframe.js
+++ piserver/public/javascripts/piframe.js
@@ -1,3 +1,33 @@
-document.onload=function(){
+window.onload=function(){
+	$('#sendfileform').on('submit', function(event){
+		event.stopPropagation();
+		event.preventDefault();
 
+		var data = new FormData();
+		$.each($('#file')[0].files, function(key, value){
+			data.append(key, value);
+		});
+
+	 	$.ajax({
+	        // url: 'http://localhost:3000/photos',
+	        type: 'POST',
+	        data: data,
+	        cache: false,
+	        dataType: 'json',
+	        processData: false, // Don't process the files
+	        contentType: 'multipart/form-data; boundary=leboundary',
+	        success: function(data, textStatus, jqXHR)
+	        {
+	        	if(typeof data.error === 'undefined'){
+	        		submitForm(event, data);
+	        	}
+	        	else{
+	        		console.log('ERRORS: ' + data.error);
+	        	}
+	        },
+	        error: function(jqXHR, textStatus, errorThrown){
+	        	console.log('ERRORS: ' + textStatus);
+	        }
+    	});
+	});
 };
