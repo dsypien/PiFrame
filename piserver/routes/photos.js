c1ddfca24ff443906b645491e663c8af255d8cbd
David Sypien <davidsypien@gmail.com>
2014-09-08 20:46:49 -0400

    Create thumbnail for each photo uplaoded

--- piserver/routes/photos.js
+++ piserver/routes/photos.js
@@ -1,8 +1,9 @@
-var express = require('express'),
-	router = express.Router();
+var express = require('express');
+var router = express.Router();
 var busboy = require('connect-busboy');
 var fs = require('fs');
 var path = require("path");
+var thumbnail = require('node-thumbnail').thumb;
 
 router.get('/', function(req, res){
 	res.render('photos', {title: 'Photos'});
@@ -11,15 +12,26 @@ router.get('/', function(req, res){
 router.post('/', function(req, res) {
 	try{
 		var fstream;
-		console.log(req);
         req.pipe(req.busboy);
         req.busboy.on('file', function (fieldname, file, filename) {
             console.log("Uploading: " + filename);
 
             //Path where image will be uploaded
-            fstream = fs.createWriteStream(path.join(__dirname, '../../pics/', filename));
+            var destPath = path.join(__dirname, '../../pics/');
+            var destThumbnails = path.join(__dirname, '../../thumbnails/');
+            var destFile = path.join(__dirname, '../../pics/', filename);
+
+            console.log(destFile);
+            fstream = fs.createWriteStream(destFile);
             file.pipe(fstream);
             fstream.on('close', function () {    
+            	// Create thumbnail;
+            	thumbnail({
+            		source: destPath,
+            		destination: destThumbnails,
+            		width: 200
+            	});
+
                 console.log("Upload Finished of " + filename);              
                 res.redirect('back');           //where to go next
             });
