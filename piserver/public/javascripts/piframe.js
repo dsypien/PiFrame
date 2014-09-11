1d3629b74ceacda2639894ddff79e67c49f65b75
David Sypien <davidsypien@gmail.com>
2014-09-11 07:17:56 -0400

    Display delete button on photo click

--- piserver/public/javascripts/piframe.js
+++ piserver/public/javascripts/piframe.js
@@ -29,4 +29,49 @@ window.onload=function(){
 	        }
     	});
 	});
+
+	$('.photo_container').bind('tap', onPhotoTap);
+
+	function onPhotoTap(){
+		$('.photo_container').removeClass('selected');
+		$(this).addClass('selected');
+
+		$('.photo_container a').removeClass('visible');
+		$(this).find('a').addClass('visible');
+	}
+
+	$('.deletebtn').click(function(){
+		var photoid= $(this).prev().attr('id');
+		alert('delete photo with id ' + photoid);
+
+		$.ajax({
+	        type: 'DELETE',
+	        cache: false,
+	        dataType: 'json',
+	        processData: false, // Don't process the files
+	        contentType: false,
+	        success: function(data, textStatus, jqXHR)
+	        {
+	        	if(typeof data.error === 'undefined'){
+	        		submitForm(event, data);
+	        	}
+	        	else{
+	        		console.log('ERRORS: ' + data.error);
+	        	}
+	        },
+	        error: function(jqXHR, textStatus, errorThrown){
+	        	console.log('ERRORS: ' + textStatus);
+	        }
+    	});
+	});
+
+
+	// $('.ui-icon-delete').mouseenter(function(){
+	// 	$('ui-icon-delete').removeClass('ui-btn-active');
+	// 	$(this).addClass('ui-btn-active');
+	// });
+
+	// $('.ui-icon-delete').mouseleave(function(){
+	// 	$(this).removeClass('ui-btn-active');
+	// });
 };
