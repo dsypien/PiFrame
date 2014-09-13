cfbe29b81c7090b1c684a9deae430ac91ffcf63c
David Sypien <davidsypien@gmail.com>
2014-09-13 07:12:50 -0400

    removing pics

--- piserver/routes/slideshows.js
+++ piserver/routes/slideshows.js
@@ -5,4 +5,16 @@ router.get('/', function(req, res){
 	res.render('slideshows', {title: 'Slideshows'});
 });
 
+router.get("/new", function(req,res){
+
+});
+
+router.get("/edit", function(req, res){
+
+});
+
+router.get("/delete", function(req,res){
+
+});
+
 module.exports = router;
