65176b722362e762a1746d7b911e3b9b9726764d
David Sypien <davidsypien@gmail.com>
2014-09-13 07:05:51 -0400

    remove ajax call to submit form

--- piserver/public/javascripts/piframe.js
+++ piserver/public/javascripts/piframe.js
@@ -1,46 +1,55 @@
 window.onload=function(){
-	$('#sendfileform').on('submit', function(event){
-		event.stopPropagation();
-		event.preventDefault();
+	//$.mobile.ajaxFormsEnabled = false;
+	// $('#sendfileform').on('submit', function(event){
+	// 	event.stopPropagation();
+	// 	event.preventDefault();
 
-		var data = new FormData();
-		$.each($('#file')[0].files, function(key, value){
-			data.append(key, value);
-		});
+	// 	var data = new FormData();
+	// 	$.each($('#file')[0].files, function(key, value){
+	// 		data.append(key, value);
+	// 	});
 
-	 	$.ajax({
-	        type: 'POST',
-	        data: data,
-	        cache: false,
-	        dataType: 'json',
-	        processData: false, // Don't process the files
-	        contentType: false,
-	        success: function(data, textStatus, jqXHR)
-	        {
-	        	if(typeof data.error === 'undefined'){
-	        		submitForm(event, data);
-	        	}
-	        	else{
-	        		console.log('ERRORS: ' + data.error);
-	        	}
-	        },
-	        error: function(jqXHR, textStatus, errorThrown){
-	        	console.log('ERRORS: ' + textStatus);
-	        }
-    	});
-	});
+	//  	$.ajax({
+	//         type: 'POST',
+	//         data: data,
+	//         cache: false,
+	//         processData: false, // Don't process the files
+	//         contentType: false,
+	//         success: function(data, textStatus, jqXHR)
+	//         {
+	//         	if(typeof data.error === 'undefined'){
+	//         		// submitForm(event, data);
+	//         		location.reload(true);
+	//         	}
+	//         	else{
+	//         		console.log('ERRORS: ' + data.error);
+	//         	}
+	//         },
+	//         error: function(jqXHR, textStatus, errorThrown){
+	//         	console.log('ERRORS: ' + textStatus);
+	//         }
+ //    	});
+	// });
+	$('.photo_container').on('click touchstart',function(){
+		var $anchorElem = $(this).find('a');
 
-	$('.photo_container').bind('tap', onPhotoTap);
+		if($anchorElem.hasClass('visible')){
+			deletePhoto.call($anchorElem);
+			return;
+		}
 
-	function onPhotoTap(){
 		$('.photo_container').removeClass('selected');
 		$(this).addClass('selected');
 
 		$('.photo_container a').removeClass('visible');
 		$(this).find('a').addClass('visible');
-	}
+		return false;
+	});
 
-	$('.deletebtn').click(function(){
+	$('.deletebtn').click(deletePhoto);
+
+	function deletePhoto(event){
+		event.stopPropagation();
 		var photoid= $(this).prev().attr('id');
 		var url = "/photos:" +photoid;
 
@@ -50,13 +59,16 @@ window.onload=function(){
 	        data: {_method: 'delete', id : photoid},
 	        success: function(data, textStatus, jqXHR)
 	        {
-	        	if(typeof data.error) {
+	        	if(data.error !== undefined) {
 	        		console.log('ERRORS: ' + data.error);
+	        	}
+	        	else{
+	        		location.reload(true);
 	        	}
 	        },
 	        error: function(jqXHR, textStatus, errorThrown){
 	        	console.log('ERRORS: ' + textStatus);
 	        }
     	});
-	});
+	}
 };
