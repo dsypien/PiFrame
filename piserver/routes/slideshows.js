4b82b50135d2a9a1976d3085249077c78c7ca816
David Sypien <davidsypien@gmail.com>
2014-09-15 07:33:23 -0400

    create routes to added pages

--- piserver/routes/slideshows.js
+++ piserver/routes/slideshows.js
@@ -6,15 +6,15 @@ router.get('/', function(req, res){
 });
 
 router.get("/new", function(req,res){
-
+	res.render('slides_new', {title: 'Add Slideshow'});
 });
 
 router.get("/edit", function(req, res){
-
+	res.render('slides_edit', {title: 'Edit Slideshow'});
 });
 
 router.get("/delete", function(req,res){
-
+	res.render('slides_delete', {title: 'Delete Slideshow'});
 });
 
 module.exports = router;
