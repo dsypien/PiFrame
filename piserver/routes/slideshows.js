707a4df6d214470e825fbcf97e186a85b7b99677
David Sypien <davidsypien@gmail.com>
2014-09-16 07:06:31 -0400

    Feed photos array to view

--- piserver/routes/slideshows.js
+++ piserver/routes/slideshows.js
@@ -6,15 +6,20 @@ router.get('/', function(req, res){
 });
 
 router.get("/new", function(req,res){
-	res.render('slides_new', {title: 'Add Slideshow'});
+	var db = req.db;
+	var collection = db.get('photo_collection');
+
+	collection.find({}, {}, function(e, docs){
+		res.render('slides_new', {title: 'Add Slide', photolist: docs});	
+	});
 });
 
 router.get("/edit", function(req, res){
-	res.render('slides_edit', {title: 'Edit Slideshow'});
+	res.render('slides_edit', {title: 'Edit Slide'});
 });
 
 router.get("/delete", function(req,res){
-	res.render('slides_delete', {title: 'Delete Slideshow'});
+	res.render('slides_delete', {title: 'Delete Slide'});
 });
 
 module.exports = router;
