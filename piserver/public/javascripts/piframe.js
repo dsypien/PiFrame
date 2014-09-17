ca07e182326fa52d361bda5444dbd2ef2ed08ed3
David Sypien <davidsypien@gmail.com>
2014-09-17 06:44:13 -0400

    call save slide api on save

--- piserver/public/javascripts/piframe.js
+++ piserver/public/javascripts/piframe.js
@@ -1,4 +1,114 @@
-window.onload=function(){
+(function(){
+	"use strict";
+
+	function init(){
+		initPhotos();
+		initSlideNew();
+	}
+
+	function initPhotos(){
+		$('.photo_container').on('click touchstart',function(){
+			var $anchorElem = $(this).find('a');
+
+			if($anchorElem.hasClass('visible')){
+				deletePhoto.call($anchorElem);
+				return;
+			}
+
+			$('.photo_container').removeClass('selected');
+			$(this).addClass('selected');
+
+			$('.photo_container a').removeClass('visible');
+			$(this).find('a').addClass('visible');
+			return false;
+		});		
+
+		$('.deletebtn').click(deletePhoto);
+
+		function deletePhoto(event){
+			event.stopPropagation();
+			var photoid= $(this).prev().attr('id');
+			var url = "/photos:" +photoid;
+
+			$.ajax({
+		        type: 'post',
+		        dataType: 'json',
+		        data: {_method: 'delete', id : photoid},
+		        success: function(data, textStatus, jqXHR)
+		        {
+		        	if(data.error !== undefined) {
+		        		console.log('ERRORS: ' + data.error);
+		        	}
+		        	else{
+		        		location.reload(true);
+		        	}
+		        },
+		        error: function(jqXHR, textStatus, errorThrown){
+		        	console.log('ERRORS: ' + textStatus);
+		        }
+	    	});
+		}
+	}
+
+	function initSlideNew(){
+		$('.photo_container_newslide').on('click touchstart', function(){
+			var $elem = $(this);
+
+			if($elem.hasClass('selected_photo')){
+				$elem.removeClass('selected_photo');
+			}
+			else{
+				$elem.addClass('selected_photo');
+			}
+		});
+
+		$('#new-slide-save').on('click touchstart', function(){
+			var selectedElements = $('#photos_list_container_newslide .selected_photo');
+			var photosAry = [];
+			var slideName = $('#slide-name').val();
+			var slideObj = {};
+
+			//validate
+			if( !slideName ){//|| !(/^\w+$/).test(slideName) 
+				alert("Please enter a name");
+			}
+
+			for(var i = 0; i < selectedElements.length; i++){
+				var imgElement = $(selectedElements[i]).children()[0];
+				var id = $(imgElement).attr('id');
+				photosAry.push(id);
+			}
+
+			slideObj.name = slideName;
+			slideObj.pictures = photosAry;
+
+			saveNewSlide(slideObj);
+			console.log(slideObj);
+		});
+
+		function saveNewSlide(slide){
+			$.ajax({
+				type: 'post',
+				data: slide,
+				dataType: 'json'
+			}).success(function(data, textStatus, jqXHR){
+
+			}).error(function(jqXHR, textStatus, errorThrwon){
+				console.log("Error uploading new slide");
+			});
+		}
+	}
+
+	return init();
+})();
+
+
+
+
+
+
+
+
 	//$.mobile.ajaxFormsEnabled = false;
 	// $('#sendfileform').on('submit', function(event){
 	// 	event.stopPropagation();
@@ -30,79 +140,3 @@ window.onload=function(){
 	//         }
  //    	});
 	// });
-	$('.photo_container').on('click touchstart',function(){
-		var $anchorElem = $(this).find('a');
-
-		if($anchorElem.hasClass('visible')){
-			deletePhoto.call($anchorElem);
-			return;
-		}
-
-		$('.photo_container').removeClass('selected');
-		$(this).addClass('selected');
-
-		$('.photo_container a').removeClass('visible');
-		$(this).find('a').addClass('visible');
-		return false;
-	});
-
-	$('.photo_container_newslide').on('click touchstart', function(){
-		var $elem = $(this);
-
-		if($elem.hasClass('selected_photo')){
-			$elem.removeClass('selected_photo');
-		}
-		else{
-			$elem.addClass('selected_photo');
-		}
-	});
-
-	$('#new-slide-save').on('click touchstart', function(){
-		var selectedElements = $('#photos_list_container_newslide .selected_photo');
-		var photosAry = [];
-		var slideName = $('#slide-name').val();
-		var slideObj = {};
-
-		//validate
-		if( !slideName || !(/^\w+$/).test(slideName) ){
-			alert("Please enter a name");
-		}
-
-		for(var i = 0; i < selectedElements.length; i++){
-			var imgElement = $(selectedElements[i]).children()[0];
-			var id = $(imgElement).attr('id');
-			photosAry.push(id);
-		}
-
-		slideObj.name = slideName;
-		slideObj.pictures = photosAry;
-
-		console.log(slideObj);
-	});
-
-	$('.deletebtn').click(deletePhoto);
-
-	function deletePhoto(event){
-		event.stopPropagation();
-		var photoid= $(this).prev().attr('id');
-		var url = "/photos:" +photoid;
-
-		$.ajax({
-	        type: 'post',
-	        dataType: 'json',
-	        data: {_method: 'delete', id : photoid},
-	        success: function(data, textStatus, jqXHR)
-	        {
-	        	if(data.error !== undefined) {
-	        		console.log('ERRORS: ' + data.error);
-	        	}
-	        	else{
-	        		location.reload(true);
-	        	}
-	        },
-	        error: function(jqXHR, textStatus, errorThrown){
-	        	console.log('ERRORS: ' + textStatus);
-	        }
-    	});
-	}
-};
