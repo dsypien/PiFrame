b4ab62acd69ed1cf925b76bcb3fb9dfc8e99c50e
David Sypien <davidsypien@gmail.com>
2014-10-01 10:02:37 -0400

    Recursively delete slide folder on delete slide and update images in slide folder on edit

--- piserver/routes/slideshows.js
+++ piserver/routes/slideshows.js
@@ -92,16 +92,17 @@ router.post("/new", function(req, res){
 	var db = req.db;
 	var photosCollection = db.get('photo_collection');
 	var slideCollection = db.get('slides_collection');
-	var destSlideDir;
 	
 	console.log	("Adding " + req.body.name);
 	console.log(req.body.pictures);
 
+    //  Get Photo Collection
 	photosCollection.find({},{},function(e1, photos){
 		if(e1){
 			console.log(e1);
 		}
 
+		// Look for duplicate slide in slide collection
 		slideCollection.find({name: req.body.name}, {}, function(e, docs){
 			//Bail if Slide with same name exists
 			if(docs.length > 0){
@@ -109,57 +110,27 @@ router.post("/new", function(req, res){
 				return;
 			}
 
-			destSlideDir = path.join(__dirname, '../../slides/', req.body.name);
-			console.log("Making the directory : " +  destSlideDir);
-			fs.mkdir(destSlideDir, 0777, function(err){
+			// Create a directory for this slide in slides
+			createSlideDirAmdSymlinks(photos, req.body);
+
+			// Save Slide to DB
+			slideCollection.insert(req.body, function(err, doc){
 				if(err){
 					console.log(err);
 				}
 
-				for(var i = 0; i < req.body.pictures.length; i ++){
-					var curId = req.body.pictures[i];
-					var curphoto;
-					var photoSearch = photos.filter(function(element){
-						return element._id == curId;
-					});
-
-					if(photoSearch.length > 0){
-						curphoto = photoSearch[0];
-					}
-					else{
-						console.log("could not find this photo in db " + curId )
-						continue;
-					}
-
-					console.log(curphoto);
-
-					var photoPath = path.join(__dirname, '../../pics/', curphoto.name);
-					var symPath = path.join(destSlideDir, curphoto.name);
-					fs.symlink(photoPath, symPath, 'file', function(symerr){
-						if(symerr){
-							console.log(symerr);
-						}
-					})
-				}
-
-				// Save Slide to DB
-				slideCollection.insert(req.body, function(err, doc){
-					if(err){
-						console.log(err);
-					}
-
-					console.log(doc);
-
-					res.send("OK");
-				});
-			})
+				console.log(doc);
+				res.send("OK");
+			});
 		});
 	});
 });
 
 router.put("/edit", function(req,res){
 	var db = req.db;
-	var collection = db.get('slides_collection');
+	var slides_collection = db.get('slides_collection');
+	var photo_collection = db.get('photo_collection');
+
 
 	var slide = {
 		_id: req.body.id,
@@ -167,28 +138,30 @@ router.put("/edit", function(req,res){
 		pictures: req.body.pictures
 	};
 
-	console.log(slide.pictures);
-	collection.update({_id: req.body.id}, slide, function(err,doc){
-		if(err){
-			console.log(err);
+	//remove symlinks
+	removeFilesInDir(req.body.name);
+
+	photo_collection.find({},{}, function(getPhotosErr, photos){
+		if(getPhotosErr){
+			console.log(getPhotosErr);
+			return;
 		}
 
-		res.send("OK");
-	});
-});
+		console.log("Phtos: " + photos);
+		console.log("Slide: " + slide);
 
-router.get("/delete", function(req,res){
-	var db = req.db;
-	var slides_collection = db.get('slides_collection');
+		//create new symlinks
+		createSymLinks(photos, slide);
 
-	slides_collection.find({},{}, function(e, docs){
-		console.log(docs);
-		
-		res.render('slides_delete', {
-			title: 'Delete Slide',
-			slides: docs
+		console.log(slide.pictures);
+		slides_collection.update({_id: req.body.id}, slide, function(err,doc){
+			if(err){
+				console.log(err);
+			}
+
+			res.send("OK");
 		});
-	});
+	})
 });
 
 router.post("/delete", function(req,res){
@@ -199,8 +172,107 @@ router.post("/delete", function(req,res){
 		if(err){
 			console.log(err);
 		}
-		res.redirect('/#slidedelete');
+
+		if(doc){
+			console.log(doc);
+		}
+
+		removeFilesInDir(req.body.name);
+		removeDir(req.body.name);
+
+		console.log(doc);
+		res.send('OK');
 	});
 });
+
+function removeFilesInDir(dirName){
+	var slidepath = path.join(__dirname, '../../slides/', dirName);
+	var curPicturePath;
+	var photoFiles;
+
+	console.log("Removing files in " + slidepath);
+
+	try{
+		photoFiles = fs.readdirSync(slidepath);
+	}
+	catch(readdirErr){
+		console.log("Could not find the directory: " + slidepath);
+		console.log(readdirErr);
+	}
+
+	if(photoFiles){
+		for(var i=0; i < photoFiles.length; i++){
+			curPicturePath = path.join(slidepath, "/", photoFiles[i]);
+
+			console.log("deleting file: " + curPicturePath);
+
+			try{
+				fs.unlinkSync(curPicturePath);
+			}
+			catch(err){
+				console.log("Error deleting picture " + photoFiles[i] + " " + err);
+			}
+		}
+	}
+}
+
+function removeDir(dir){
+	var slidepath = path.join(__dirname, '../../slides/', dir);
+
+	fs.rmdir(slidepath, function(remErr){
+		if(remErr){
+			console.log(remErr);
+		}
+		else{
+			console.log("File successfully deleted");
+		}
+	});
+}
+
+function createSlideDirAmdSymlinks(photos, slide){
+	var destSlideDir = path.join(__dirname, '../../slides/', slide.name);
+	console.log("Making the directory : " +  destSlideDir);
+	
+	fs.mkdir(destSlideDir, 0777, function(err){
+		if(err){
+			console.log(err);
+		}
+
+		createSymLinks(photos, slide);
+	})
+}
+
+function createSymLinks(photos, slide){
+	console.log("Creating symlinks for slide :" + slide);
+	var destSlideDir = path.join(__dirname, '../../slides/', slide.name);
+
+	// Create Symlinks for each photo in the photo array
+	for(var i = 0; i < slide.pictures.length; i ++){
+		var curId = slide.pictures[i];
+		var curphoto;
+		// Get photo array that should be of size one and contain the photo
+		var photoSearch = photos.filter(function(element){
+			return element._id == curId;
+		});
+
+		if(photoSearch.length > 0){
+			curphoto = photoSearch[0];
+		}
+		else{
+			console.log("could not find this photo in db " + curId );
+			continue;
+		}
+
+		console.log(curphoto);
+
+		var photoPath = path.join(__dirname, '../../pics/', curphoto.name);
+		var symPath = path.join(destSlideDir, curphoto.name);
+		fs.symlink(photoPath, symPath, 'file', function(symerr){
+			if(symerr){
+				console.log(symerr);
+			}
+		})
+	}
+}
 
 module.exports = router;
