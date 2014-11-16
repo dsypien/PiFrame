2b31eee4e7ce0249cbfc7dd4a313017a655711e5
David Sypien <davidsypien@gmail.com>
2014-11-16 17:12:05 -0500

    remove implementation from photo route

--- piserver/routes/photos.js
+++ piserver/routes/photos.js
@@ -22,58 +22,20 @@ router.get('/json', function(req,res){
 
 router.post('/', function(req, res) {
 	console.log(req.headers);
-	var fstream,
-		db = req.db,
-		photos,
-		insert_statement;
-
 
 	if(req.body._method === 'delete'){
-		console.log("Deleteing pic with id " + req.body.id);
-		
-		//extract file data
-		collection.find({_id: req.body.id}, function(err, doc){
+		photos.remove(req, function(err, docs){
 			if(err){
-				res.json(500, error);
+				console.log(err);
+				res.send(err);
 			}
 			else{
-				console.log(doc);
-				//Remove from db
-				collection.remove({_id: req.body.id}, function(err){
-					if(err){
-						res.json(500, error);
-					}
-					else{
-						//Remove file
-						fs.unlink(path.join(__dirname, '../../pics/', doc[0].name), function(delErr){
-							if(delErr){
-								console.log(delErr);
-							}
-							else{
-								console.log("succesfully deleted " + doc[0].name );
-							}
-						});
-						//Remove thumbnail
-						fs.unlink(path.join(__dirname, '../public/thumbnails/', doc[0].thumb_name), function(delErr){
-							if(delErr){
-								console.log(delErr);
-								res.send(delErr);
-							}
-							else{
-								console.log("succesfully deleted" +  doc[0].thumb_name );
-								//res.redirect("/#photos");
-								collection.find({}, {}, function(e, docs){
-									res.json(docs);
-								});
-							}
-						});
-					}
-				});
+				res.json(docs);
 			}
 		});
 	}
 	else{
-		photos.addPhoto(req, function(err){
+		photos.add(req, function(err){
 			if(err){
 				console.log(err);
 				res.redirect(req.get('referer'));
