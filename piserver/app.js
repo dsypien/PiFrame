6a79ff54c58c5769fe7ba567908587b4578a98b2
David Sypien <sypiend@mskcc.org>
2015-04-18 07:41:37 -0400

    set new favicon

--- piserver/app.js
+++ piserver/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var fs = require('fs');
@@ -22,7 +22,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+app.use(favicon(__dirname + '/public/images/piframe.ico') );
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser({uploadDir:'./pics'}));
