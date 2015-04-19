ca3bd46d1dfa7ab28e2d5fd3db1cb196e0c2b0b7
David Sypien <sypiend@mskcc.org>
2015-04-19 15:22:50 -0400

    Add comments to qiv script

--- startslides.sh
+++ startslides.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+
+# Kill all instances of qiv first
 killall qiv
+
+# Start the slide with the following options:
+# -f  Fullscren
+# -s  Start slideshow immediately
+# -t  Scale larger images down to fit
+# -i  Disable statusbar
 qiv -fsti PiFrame/slides/"$1"/*.jpg
