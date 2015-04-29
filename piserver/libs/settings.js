8497078d60d448c5dc1c9e59abd487249f2e7c70
David Sypien <sypiend@mskcc.org>
2015-04-29 04:47:42 -0400

    spawning slideshow script

--- piserver/libs/settings.js
+++ piserver/libs/settings.js
@@ -1,8 +1,9 @@
 var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var execFile = require('child_process').execFile;
 var spawn = require('child_process').spawn;
 var path = require('path');
-var script_path = path.join(__dirname, "../../startslides.sh");//path.join(__dirname, "../../test.sh");
+var script_path = path.join(__dirname, "../../startslides.sh");
 
 module.exports = function(){
 	function play(slide, callback){
@@ -14,23 +15,35 @@ module.exports = function(){
 			callback(errMsg);
 		}
 
-		script_w_param = script_path + " " + slide.name;
+		var qiv = spawn('script_path', [slide.name]);
 
-		var spwan(script_path, slide.name);
-
-		, function(error, stdout, stderr){
-			console.log('stdout: ' + stdout);
-		    console.log('stderr: ' + stderr);
-		    if (error !== null) {
-		    	errMsg = 'exec error: ' + error;
-			    console.log(errMsg);
-			    callback(errMsg);
-			    return;
-		    }
-
-		    console.log("Playing slide: " + slide.name);
-			callback();
+		qiv.stdout.on('data', function (data) {
+		  console.log('stdout: ' + data);
 		});
+
+		qiv.stderr.on('data', function (data) {
+		  console.log('stderr: ' + data);
+		});
+
+		qiv.on('close', function (code) {
+		  console.log('child process exited with code ' + code);
+		});
+
+		// script_w_param = script_path + " " + slide.name;
+		//
+		// exec(script_w_param, function(error, stdout, stderr){
+		// 	console.log('stdout: ' + stdout);
+		//     console.log('stderr: ' + stderr);
+		//     if (error !== null) {
+		//     	errMsg = 'exec error: ' + error;
+		// 	    console.log(errMsg);
+		// 	    callback(errMsg);
+		// 	    return;
+		//     }
+
+		//     console.log("Playing slide: " + slide.name);
+		// 	callback();
+		// });
 	}
 
 	return{
