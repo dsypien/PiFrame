0037b508da179a903f56cda7f958733ba5ee8250
David Sypien <sypiend@mskcc.org>
2015-04-28 22:13:00 -0400

    Update gulp builds

--- piserver/gulpfile.js
+++ piserver/gulpfile.js
@@ -5,42 +5,63 @@ gulp.task('default',
 );
 
 gulp.task('build', 
+	['clean', 'lint', 'compile']
+);
+
+gulp.task('build-dist', 
 	['clean', 'lint', 'compile-dist']
 );
 
 gulp.task('clean', function(){
 	var del = require('del');
-
-	del.sync(['./dev/build/**/*']);
+	del.sync(['./public/css/*', './public/js/*', './public/images/*']);
 });
 
 gulp.task('lint', function(){
 	var jshint = require('gulp-jshint');
 
-	gulp.src(['./dev/js/*.js'])
+	gulp.src(['./public/js/*.js'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(jshint.reporter('fail'));
 });
 
 gulp.task('compile', function(){
-	var browserify = require('gulp-browserify'),
-        concat = require('gulp-concat');
+	var concat = require('gulp-concat');
+	gulp.src(['./components/js/*.js'])
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./public/js'));
+
+	gulp.src(['./components/css/*.css'])
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('./public/css'));
+
+	gulp.src(['./components/images/*'])
+		.pipe(gulp.dest('./public/images/'));
 });
 
 gulp.task('compile-dist', function(){
-	var browserify = require('gulp-browserify'),
-        concat = require('gulp-concat'),
+	var concat = require('gulp-concat'),
         stripDebug = require('gulp-strip-debug'),
-		uglify = require('gulp-uglify');
+		uglify = require('gulp-uglify'),
+		minify = require('gulp-minify-css'),
+		imageOp = require('gulp-image-optimization');
 
-	gulp.src(['./dev/js/main.js'])
-	    .pipe(browserify({
-            debug: false
-        }))
-        .pipe(concat('bundled.js'))
+	gulp.src(['./components/js/main.js'])
+		.pipe(concat('main.js'))
 		.pipe(uglify())
 		.pipe(stripDebug())
-		.pipe(gulp.dest('./dev/build/js'));
+		.pipe(gulp.dest('./public/js'));
+
+	gulp.src(['./components/css/style.css'])
+		.pipe(concat('style.css'))
+		.pipe(minify())
+		.pipe(gulp.dest('./public/css'));
+
+	gulp.src(['./components/images/*'])
+		.pipe(gulp.dest('./public/images/'));
+
+	gulp.src(['./components/images/*.ico'])
+		.pipe(gulp.dest('./public/images/'));
 
 });
