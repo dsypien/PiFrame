39195d8095ed338f52db6d79bb7f048981f8f5ae
David Sypien <sypiend@mskcc.org>
2015-05-01 18:38:19 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Crate a directory for the project
+mkdir ~/Projects
+
+# Step into the project directory
+cd Projects
+
+# Clone the repository
+git clone https://github.com/dsypien/PiFrame.git
+
 # Get & install ARM version of Node
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
 dpkg -i node_latest_armhf.deb
@@ -9,6 +18,11 @@ apt-get install sqlite3
 apt-get install imagemagick
 apt-get install qiv
 npm install -g bower
+npm install -g gulp
 
+# Run updates
 npm update
 bower update
+
+# Build project
+gulp build-dist
