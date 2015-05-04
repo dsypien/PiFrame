c66beeedb9401ba40a64ffc2c622ab5062205826
David Sypien <sypiend@mskcc.org>
2015-05-04 00:18:26 -0400

    update script

--- startpiserver.sh
+++ startpiserver.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
 # Crate a directory for the project
-mkdir ~/Projects
-
-# Step into the project directory
-cd Projects
-
-# Clone the repository
-git clone https://github.com/dsypien/PiFrame.git
-
-# Get & install ARM version of Node
-wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
-dpkg -i node_latest_armhf.deb
-
-# Install other dependencies
-apt-get install sqlite3
-apt-get install imagemagick
-apt-get install qiv
-npm install -g bower
-npm install -g gulp
-
-# Run updates
-npm update
-bower update
-
-# Build project
-gulp build-dist
+npm start --prefix Projects/PiFrame/piserver
