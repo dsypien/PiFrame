71c88d875858d153bffcaf706b012ae0dd8e8ad4
David Sypien <davidsypien@gmail.com>
2015-10-17 21:21:04 -0400

    update script to launch terminal and run piserver

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
 # Run server if port available
-if netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".10239"'
-then
-
-	npm start --prefix /home/pi/Projects/PiFrame/piserver
-
-else
-
-	echo port 10239 not available
-
-fi
+lxterminal -e npm start --prefix /home/pi/Projects/PiFrame/piserver
