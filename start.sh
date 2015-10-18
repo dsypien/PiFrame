e1ef218cc3505fb82bd53bf2c820db8c9c8a5547
David Sypien <davidsypien@gmail.com>
2015-10-17 20:54:17 -0400

    Run server if port 10239 is open

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
-# Create a directory for the project
-npm start --prefix /home/pi/Projects/PiFrame/piserver
+# Run server if port available
+if netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".10239"'
+then
+
+	npm start --prefix /home/pi/Projects/PiFrame/piserver
+
+else
+
+	echo port 10239 not available
+
+fi
